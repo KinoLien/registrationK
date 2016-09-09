ef3d0f1974dc79494c9aae5b4e1a153b1d97ed14
KinoLien <miecowbai@gmail.com>
2016-09-09 20:08:14 +0800

    Revise the id validation: allow 6 to 10 length.

--- first_visit.php
+++ first_visit.php
@@ -74,16 +74,16 @@
         var id = o.value;
         if (id == "") resMsg = "請輸入身分證字號！";
         else if (id.length > 10) resMsg = "身分證字號請勿大於十碼！";
-        else if(id.length < 10) resMsg = "身分證字號請勿小於十碼！";
+        else if(id.length < 6) resMsg = "身分證字號請勿小於六碼！";
         else if($("input[name=_idValid]").val() == '0') resMsg = "身分證字號已經存在！";
         else if (!(id.charAt(0)>='A' && id.charAt(0)<='Z')) resMsg = "身分證字號第一碼必須是大寫的英文字母！";
         else if (!(id.charAt(1)>='1' && id.charAt(1)<='2')) resMsg = "身分證字號第二碼必須是數字1或2！";
         else{
-          for (var i = 1; i < 10; i++) {
-            if (!(id.charAt(i)>='0' && id.charAt(i)<='9')) resMsg = "身分證字號第二碼至第十碼必須為數字！";
+          for (var i = 1; i < id.length; i++) {
+            if (!(id.charAt(i)>='0' && id.charAt(i)<='9')) resMsg = "身分證字號第二碼至最後一碼必須為數字！";
           }
         }
-        if(!resMsg && !checkTwID(id)) resMsg = "身分證字號不符合產出公式 (Fake)";
+        // if(!resMsg && !checkTwID(id)) resMsg = "身分證字號不符合產出公式 (Fake)";
 
         if(resMsg) valid = false;
         return { msg: resMsg, valid:valid };
@@ -213,7 +213,7 @@
       		</p>
       		<p>
       			<strong>身分證字號</strong>：
-      			<input name="id" type="text" placeholder="含英文字母共十碼">&nbsp;*
+      			<input name="id" type="text" placeholder="含英文字母共六碼以上至十碼">&nbsp;*
             <input name="_idValid" type="hidden">
             <span class="glyphicon glyphicon-ok" style="color: #5cb85c; display: none;">身分證OK</span>
             <span class="glyphicon glyphicon-remove" style="color: #d9534f; display: none;">身分證字號重複</span>
